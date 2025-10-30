04c8d37188f30d9185cccea8a7bebcdd49353d4d
Mariam Ayman <mariam.meligy02@gmail.com>
2025-10-30 23:25:16 +0200

    task03

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,33 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
-  <body>
 
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">task1</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Table</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
-<div class="mt-4">
+<!-- <div class="mt-4">
     <button type="button" class="btn btn-success">add row</button>
-</div>
-
+</div> -->
+@extends('layouts.app')
+@section('content')
+@section('title', 'Posts Table')
     <div class="container mt-5">
         <table class="table table-bordered">
             <thead>
@@ -52,7 +30,7 @@
                 </tr>
                 @endforeach
             </tbody>
-        </table>    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
-</html>
+        </table>   
+      </div>
+
+@endsection 
